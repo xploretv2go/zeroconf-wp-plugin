712d79b1e761c9d6426384eb15b646c2b6d0af08
Frantisek Farkas <frantisekfarkas93@gmail.com>
2021-12-02 22:53:23 +0100

    Add files via upload

---
+++ build/precache-manifest.98eb5317d57e4562199309ac2b29ba0a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e60d59b69f9404328416271713da0934",
+    "url": "/wordpress/wp-content/reactpress/apps/zeroconf-wp-plugin/build/index.html"
+  },
+  {
+    "revision": "cf6e52fc1b754c3195bc",
+    "url": "/wordpress/wp-content/reactpress/apps/zeroconf-wp-plugin/build/static/css/main.4e14b829.chunk.css"
+  },
+  {
+    "revision": "6c8a2f2aa2c0bdd191ad",
+    "url": "/wordpress/wp-content/reactpress/apps/zeroconf-wp-plugin/build/static/js/2.445de47d.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/wordpress/wp-content/reactpress/apps/zeroconf-wp-plugin/build/static/js/2.445de47d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf6e52fc1b754c3195bc",
+    "url": "/wordpress/wp-content/reactpress/apps/zeroconf-wp-plugin/build/static/js/main.1a912471.chunk.js"
+  },
+  {
+    "revision": "7a3d5d14d81da307fb08",
+    "url": "/wordpress/wp-content/reactpress/apps/zeroconf-wp-plugin/build/static/js/runtime-main.0b1b1bcf.js"
+  }
+]);
---
+++ build/service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/wordpress/wp-content/reactpress/apps/zeroconf-wp-plugin/build/precache-manifest.98eb5317d57e4562199309ac2b29ba0a.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/wordpress/wp-content/reactpress/apps/zeroconf-wp-plugin/build/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
